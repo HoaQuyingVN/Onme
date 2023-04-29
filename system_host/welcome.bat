3d582c5d3670c09d454cc2801399bc11b37c1884
HoaQuyingVN <126839568+HoaQuyingVN@users.noreply.github.com>
2023-04-29 10:19:47 +0700

    Create welcome.bat

---
+++ system_host/welcome.bat
@@ -0,0 +1,8 @@
+@echo off
+echo Welcome to Onme Desktop!
+echo.
+echo Congratulations, Onme Desktop has been successfully installed on your computer.
+echo.
+echo Take a moment to explore the new features of Onme Desktop and customize your desktop according to your preferences.
+echo.
+echo Thank you for choosing Onme Desktop!
